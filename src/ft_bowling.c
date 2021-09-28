53fa55a3a667cd990773e59e1310600c230d83d0
Yohan Foucade <yohan.foucade@gmail.com>
2021-09-28 19:39:28 +0200

    test spare at the end

--- src/ft_bowling.c
+++ src/ft_bowling.c
@@ -1,7 +1,11 @@
 #include <stddef.h>
 
+#define TRUE 1
+#define FALSE 0
+
 int	roll_count = 0;
 int history[21];
+int	valid = TRUE;
 
 void init()
 {
@@ -11,14 +15,31 @@ void init()
 
 void	roll(int knocked_pins)
 {
+	if (roll_count >= 21) {
+		valid = FALSE;
+		return ;
+	}
 	history[roll_count] = knocked_pins;
 	roll_count++;
 }
 
+// 5 5
+// 15
+
+// 5 5   1
+// 10+1  1
+
 int score(void)
 {
-	int total_score;
+	int total_score = 0;
+
+	if (!valid)
+		return (-1);
 	for (roll_count = 0; roll_count < 21; roll_count++)
+	{
+		if (roll_count % 2 && history[roll_count - 1] + history[roll_count] == 10)
+			total_score += history[roll_count + 1];
 		total_score += history[roll_count];
+	}
 	return total_score;
 }
