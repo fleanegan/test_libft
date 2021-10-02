d56a4e6076dc4cdb0694bb83f2f0a63bb8267bc7
Yohan Foucade <yohan.foucade@gmail.com>
2021-10-02 14:17:15 +0200

    atoi

--- src/libft.h
+++ src/libft.h
@@ -1,20 +1,89 @@
 #ifndef LIBFT_H
 # define LIBFT_H
-# include <stddef.h>
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isupper(int c);
-int		ft_islower(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
+
+# include <stdlib.h>
+
+// https://code.woboq.org/gcc/gcc/ginclude/stddef.h.html
+
+// part 1
+int		ft_atoi(const char *nptr);
 void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-char	*ft_toupper(char *input);
-char	*ft_tolower(char *input);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+void	*ft_memset(void *s, int c, size_t n);
+char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
-#endif //LIBFT_H
+char	*ft_strdup(const char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(char *haystack, char *needle, size_t n);
+char	*ft_strrchr(const char *s, int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+// part 2
+char	*ft_itoa(int n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char	**ft_split(char const *s, char c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));;
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+// bonus part
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
+
+// personal functions
+char	ft_sign(long nb);
+char	*ft_strtrim_char(char const *s1, char c);
+long	ft_get_signed_max(size_t nbits);
+long	ft_get_signed_min(size_t nbits);
+int		ft_isspace(int c);
+
+// get_next_line
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1023
+# endif
+
+# define MAX_FILES 1024
+
+typedef struct s_gnl_buffer
+{
+	size_t		start;
+	char		buf[BUFFER_SIZE + 1];
+}	t_gnl_buffer;
+
+int		get_next_line(int fd, char **line);
+
+// todo: ft_lstsort, ft_lst_issorted, ft_lst_find, 
+
+#endif
---
+++ test/test_ft_atoi.h
@@ -0,0 +1,103 @@
+#include <criterion/criterion.h>
+#include "../src/libft.h"
+
+int	ft_atoi(const char *in)
+{
+	int	sign;
+	int	res;
+
+	res = 0;
+	sign = 1;
+	while (ft_isspace(*in))
+		in++;
+	if (*in == '-' || *in == '+')
+	{
+		if (*in == '-')
+			sign = -1;
+		in++;
+	}
+
+	while (ft_isdigit(*in))
+		res = 10 * res + (*in++ - '0');
+	return (sign * res);
+}
+
+
+Test(test_atoi, empty_string) {
+	char *in = "";
+	int exp_res = atoi(in);
+
+	int res = ft_atoi(in);
+
+	cr_assert_eq(exp_res, res, "expected: %d , real: %d", exp_res, res);
+}
+
+Test(test_atoi, invalid_input) {
+	char *in = "j";
+	int exp_res = atoi(in);
+
+	int res = ft_atoi(in);
+	
+	cr_assert_eq(exp_res, res, "expected: %d , real: %d", exp_res, res);
+}
+
+Test(test_atoi, signle_digit_input) {
+	char *in = "1";
+	int exp_res = atoi(in);
+
+	int res = ft_atoi(in);
+	
+	cr_assert_eq(exp_res, res, "expected: %d , real: %d", exp_res, res);
+}
+
+Test(test_atoi, signle_digit_input_with_signs) {
+	char *in1 = "-1";
+	int exp_res1 = atoi(in1);
+	int res1 = ft_atoi(in1);
+	char *in2 = "+1";
+	int exp_res2 = atoi(in2);
+	int res2 = ft_atoi(in2);
+
+	cr_assert_eq(exp_res1, res1, "expected: %d , real: %d", exp_res1, res1);
+	cr_assert_eq(exp_res2, res2, "expected: %d , real: %d", exp_res2, res2);
+}
+
+Test(test_atoi, whitespaces_at_begining) {
+	char *in1 = "	 -1";
+	int exp_res1 = atoi(in1);
+
+	int res1 = ft_atoi(in1);
+
+	cr_assert_eq(exp_res1, res1, "expected: %d , real: %d", exp_res1, res1);
+}
+
+Test(test_atoi, multiple_digits) {
+	char *in1 = "123";
+	int exp_res1 = atoi(in1);
+
+	int res1 = ft_atoi(in1);
+
+	cr_assert_eq(exp_res1, res1, "expected: %d , real: %d", exp_res1, res1);
+}
+
+Test(test_atoi, overflow_with_normal_int) {
+	char *in1 = "-2147483648";
+	long exp_res1 = atoi(in1);
+
+	long res1 = ft_atoi(in1);
+
+	cr_assert_eq(exp_res1, res1);//, "expected: %d , real: %d", exp_res1, res1);
+}
+
+Test(test_atoi, undefined_behavior) {
+	char *in1 = "-21474836481";
+	int exp_res1 = atoi(in1);
+
+	int res1 = ft_atoi(in1);
+
+	cr_assert_eq(exp_res1, res1);//, "expected: %d , real: %d", exp_res1, res1);
+}
+
+
+
+// what else !?
